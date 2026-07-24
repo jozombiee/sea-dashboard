3ddff388583e111404bac13ad42a9fad58ca5539
young <89303176+jozombiee@users.noreply.github.com>
2026-07-24 14:28:24 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="sea-v2";const A=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>{if(e.request.url.includes("open-meteo.com"))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
